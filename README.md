6ebd4a416f108906ac4a9128cd0238f05c590c0d
dantaki <dantaki@ucsd.edu>
2017-05-12 12:47:12 -0700

    :frog:

--- README.md
+++ README.md
@@ -1,2 +1,86 @@
 # rgged
 Alter or Add Read Group Tags to BAM Files
+
+## Install
+
+#### Clone from GitHub
+
+```
+git clone --recursive https://github.com/dantaki/rgged.git
+```
+
+#### Compile with [CMake](https://cmake.org/)
+
+```
+cd rgged/
+mkdir build
+cd build/
+
+cmake .. 
+
+make 
+```
+
+##### Binary executable found under `chromCov/build/src/chromCov`
+
+## Usage 
+
+`rgged --help`
+
+```
+rgged        Alter or Add Read Group (RG) Information
+
+---------------------------------------------------
+ WARNING!     Existing Read Groups will be erased!
+---------------------------------------------------
+
+Version: 1.0	Author: Danny Antaki <dantaki@ucsd.edu>
+
+Usage: rgged -i <in.bam> -id <ID> -lb <LIBRARY> -pl <PLATFORM> -sm <SAMPLE> -o <output.bam>
+
+Options:
+    -i        Input: BAM filename
+    -id       ID
+    -lb       Library
+    -pl       Platform
+    -sm       Sample
+    -o        Output: BAM filename [Default: in_rgged.bam]
+```
+
+## Acknowledgements
+
+rgged uses [BamTools](https://github.com/pezmaster31/bamtools) and [zlib](http://zlib.net/)
+
+> *BamTools*
+> Copyright (c) 2009-2010 Derek Barnett, Erik Garrison, Gabor Marth, Michael Stromberg
+
+> *zlib*
+> Copyright (C) 1995-2012 Jean-loup Gailly and Mark Adler
+
+## License
+
+MIT License
+
+Copyright (c) 2017 Danny Antaki
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+## Contact
+
+dantaki@ucsd.edu
---
+++ src/rgged.cc
@@ -0,0 +1,91 @@
+#include "api/BamReader.h"
+#include "api/BamWriter.h"
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+using namespace BamTools;
+using namespace std;
+inline vector<string> split(string &s, char delim) 
+{
+  vector<string> v;
+  stringstream ss(s); string i; 
+  while(getline(ss, i, delim)) { v.push_back(i); }
+  return v;
+}
+inline int32_t rightPos(int32_t &pos, vector<CigarOp> &cigar){
+	int len=0;
+	for(vector<CigarOp>::iterator it=cigar.begin(); it != cigar.end(); ++it){
+		if(it->Type == 'D' || it->Type == 'M' || it->Type == '=' || it->Type == 'X') { len+=it->Length; }
+	}
+	len+=pos;
+	return len;
+}
+inline vector<CigarOp> rollCig(string &cigar) {
+	vector<CigarOp> splitCigar;
+	istringstream parser(cigar);
+	char sC; uint32_t sL;
+	while(parser >> sL >> sC) { splitCigar.push_back(CigarOp(sC, sL)); }
+	return splitCigar;
+}
+int main(int argc, char *argv[])
+{
+	const string splash="rgged        Alter or Add Read Group (RG) Information\n"
+		       "\n---------------------------------------------------\n" 
+			 " WARNING!     Existing Read Groups will be erased! "
+		       "\n---------------------------------------------------\n" 
+			  "\nVersion: 1.0	Author: Danny Antaki <dantaki@ucsd.edu>\n\n"
+			    "Usage: rgged -i <in.bam> -id <ID> -lb <LIBRARY> -pl <PLATFORM> -sm <SAMPLE> -o <output.bam>\n\nOptions:\n"
+				"    -i        Input: BAM filename\n"
+				"    -id       ID\n"
+				"    -lb       Library\n"
+				"    -pl       Platform\n"
+				"    -sm       Sample\n"
+				"    -o        Output: BAM filename [Default: in_rgged.bam]\n\n";
+	string ifh;
+	string ID; string LB; string PL; string SM; 
+	string ofh; string args=string(argv[0]);
+	if ( (argc==1) ||
+	     (argc==2 && string(argv[1]) == "-h") ||
+	     (argc==2 && string(argv[1]) == "-help") ||
+	     (argc==2 && string(argv[1]) == "--help")) {
+		cout << "" << endl; cout << splash << endl;
+		return 1;
+	}
+	for(int i=1; i<argc; ++i){
+		if(string(argv[i]) == "-i" || string(argv[i]) == "--in" || string(argv[i]) == "-in"){ ifh = string(argv[i+1]); i++; continue; }
+		if(string(argv[i]) == "-id" || string(argv[i]) == "--id" || string(argv[i])=="--ID" || string(argv[i])=="-ID"){ ID = string(argv[i+1]); i++; continue; }
+		if(string(argv[i]) == "-lb" || string(argv[i]) == "--lb" || string(argv[i])=="--LB" || string(argv[i])=="-LB"){ LB = string(argv[i+1]); i++; continue; }
+		if(string(argv[i]) == "-pl" || string(argv[i]) == "--pl" || string(argv[i])=="--PL" || string(argv[i])=="-PL"){ PL = string(argv[i+1]); i++; continue; }
+		if(string(argv[i]) == "-sm" || string(argv[i]) == "--sm" || string(argv[i])=="--SM" || string(argv[i])=="-SM"){ SM = string(argv[i+1]); i++; continue; }
+		if(string(argv[i]) == "-o" || string(argv[i]) == "--out" || string(argv[i]) == "-out"){ ofh = string(argv[i+1]); i++; continue; }
+		cerr << "ERROR: Unknown option "<<string(argv[i])<< "\n\n"<< splash << endl;
+		return 1;
+	} for(int i=1; i<argc; ++i){ args=args+" "+argv[i]; }
+	if(ifh == "") { cerr << "ERROR: No BAM file given\n\n"<< splash << endl; return 1; }
+	if(ID == "" ) { cerr << "ERROR: No ID <-id [ID]>\n\n"<< splash << endl; return 1; } 
+	if(ofh == ""){ ofh = ifh.substr(0,ifh.find_last_of('.'))+"_rgged.bam";  }
+	BamReader reader; BamWriter out;
+	if (!reader.Open(ifh)){ cerr << "ERROR: " << ifh << " could not be opened!\n\n"<< splash << endl; return 1; }
+	BamAlignment al;
+	const RefVector refs = reader.GetReferenceData();
+	SamHeader head = reader.GetHeader();
+	SamProgram rgged; rgged.CommandLine = args; rgged.ID="rgged";
+	head.Programs.Add(rgged); 
+	SamReadGroup myRG; myRG.ID=ID; if(SM=="") { SM=ID; myRG.Sample=SM;} 
+	if(LB!="") { myRG.Library=LB;} 
+	if(PL!="") { myRG.SequencingTechnology=PL;}
+	SamReadGroupDictionary rg; rg.Add(myRG);
+	head.ReadGroups=rg;
+	if(!out.Open(ofh,head,refs)){ cerr << "ERROR: " << ofh << " could not be opened for writing\n\n"<< splash << endl; return 1; } 
+	while (reader.GetNextAlignmentCore(al)){
+		al.BuildCharData();
+		if(al.HasTag("RG")) { al.RemoveTag("RG"); } 
+		al.AddTag("RG","Z",ID);
+		out.SaveAlignment(al);
+	}
+	reader.Close(); out.Close();
+	return 0;
+}
