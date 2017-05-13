66550931e2f73965281168ee79bf673fa673fea3
dantaki <dantaki@ucsd.edu>
2017-05-13 10:37:56 -0700

    k

--- build/lib/zlib-1.2.7/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dantakli/rgged/lib/zlib-1.2.7
-# Build directory: /home/dantakli/rgged/build/lib/zlib-1.2.7
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
